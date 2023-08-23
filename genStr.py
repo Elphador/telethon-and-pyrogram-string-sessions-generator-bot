780340279617bfcd5aec83ec66d3bcfaaaa4c2f8
Elphador <96861627+Elphador@users.noreply.github.com>
2023-08-24 00:36:17 +0300

    Add files via upload

--- genStr.py
+++ genStr.py
@@ -1,3 +1,14 @@
+from telethon import TelegramClient
+from asyncio.exceptions import TimeoutError
+from telethon.sessions import StringSession
+from telethon.errors import (
+    ApiIdInvalidError,
+    PhoneNumberInvalidError,
+    PhoneCodeInvalidError,
+    PhoneCodeExpiredError,
+    SessionPasswordNeededError,
+    PasswordHashInvalidError
+)
 import asyncio,os
 from bot import bot
 from pyroaddon import listen
@@ -11,7 +22,7 @@ from pyrogram.errors import (
 )
 
 API_TEXT = """Hi, {}.
-This is Pyrogram's String Session Generator Bot. I will generate String Session of your Telegram Account.
+This is Pyrogram and telethon  String Session Generator Bot. I will generate String Session of your Telegram Account.
 
 By @Neuralp
 
@@ -22,8 +33,84 @@ PHONE_NUMBER_TEXT = (
     "Including Country code. Example: **+14154566376**\n\n"
     "Press /cancel to Cancel Task."
 )
+@bot.on_message(filters.private & filters.command('telethon'))
+async def tele(c,m):
+    chat = m.chat
+    api = await bot.ask(
+        chat.id, API_TEXT.format(m.from_user.mention)
+    )
+    if await is_cancel(m, api.text):
+        return
+    try:
+        check_api = int(api.text)
+    except Exception:
+        await m.reply("`API_ID` is Invalid.\nPress /start to Start again.")
+        return
+    api_id = api.text
+    hash = await bot.ask(chat.id, HASH_TEXT)
+    if await is_cancel(m, hash.text):
+        return
+    if not len(hash.text) >= 30:
+        await m.reply("`API_HASH` is Invalid.\nPress /start to Start again.")
+        return
+    api_hash = hash.text
+    while True:
+        number = await bot.ask(chat.id, PHONE_NUMBER_TEXT)
+        if not number.text:
+            continue
+        if await is_cancel(m, number.text):
+            return
+        phone = number.text
+        confirm = await bot.ask(chat.id, f'`Is "{phone}" correct? (y/n):` \n\nSend: `y` (If Yes)\nSend: `n` (If No)')
+        if await is_cancel(m, confirm.text):
+            return
+        if "y" in confirm.text:
+            break
+    try :
+        client = TelegramClient(StringSession(), api_id, api_hash)
+        await client.connect()
+    except Exception as e:
+        await m.reply(e)
+    try :
+        code = await client.send_code_request(phone)
+    except (ApiIdInvalid, ApiIdInvalidError):
+        await m.reply('`API_ID` and `API_HASH` combination is invalid. Please start generating session again.')
+        return
+    except (PhoneNumberInvalid, PhoneNumberInvalidError):
+        await m.reply('`PHONE_NUMBER` is invalid. Please start generating session again.')
+        return
+    try:
+        code= await bot.ask(chat.id, "Please check for an OTP in official telegram account. If you got it, send OTP here after reading the below format. \nIf OTP is `12345`, **please send it as** `1 2 3 4 5`.", filters=filters.text, timeout=600)
+    except TimeoutError:
+        await m.reply('Time limit reached of 10 minutes. Please start generating session again.')
+        return
+    try :
+        code = code.text 
+        code = ' '.join(str(code))
+        await client.sign_in(phone, code, password=None)
+    except (PhoneCodeInvalid, PhoneCodeInvalidError):
+        await m.reply('OTP is invalid. Please start generating session again.')
+        return
+    except (PhoneCodeExpired, PhoneCodeExpiredError):
+        await m.reply('OTP is expired. Please start generating session again.')
+        return
+    except (SessionPasswordNeeded, SessionPasswordNeededError): 
+        try:
+            two_step= await bot.ask(chat.id, 'Your account has enabled two-step verification. Please provide the password.', filters=filters.text, timeout=300)
+        except TimeoutError:
+            await m.reply('Time limit reached of 5 minutes. Please start generating session again.')
+            return
+        try:
+            password = two_step.text
+            await client.sign_in(password=password)
+        except (PasswordHashInvalid, PasswordHashInvalidError):
+            await m.reply('Invalid Password Provided. Please start generating session again.', quote=True)
+            return
+    string_session = client.session.save()
+    await m.reply(f'copy it `{string_session}`')
+            
 
-@bot.on_message(filters.private & filters.command("start"))
+@bot.on_message(filters.private & filters.command("pyrogram"))
 async def genStr(_, msg: Message):
     chat = msg.chat
     api = await bot.ask(
@@ -122,18 +209,8 @@ async def genStr(_, msg: Message):
         return
     try:
         session_string = await client.export_session_string()
-        session_string = str(session_string)
-        try:
-            await msg.reply(session_string)
-        except Exception as e :
-            await msg.reply(e)
-        await client.send_message("me", f"#STRING \n {session_string} \n")
+        await msg.reply(f'click to copy `{str(session_string)}`')
         await client.disconnect()
-        text = "String Session is Successfully Generated.\nClick on Below Button."
-        reply_markup = InlineKeyboardMarkup(
-            [[InlineKeyboardButton(text="Show String Session", url=f"tg://openmessage?user_id={chat.id}")]]
-        )
-        await bot.send_message(chat.id, text, reply_markup=reply_markup)
         os.remove("my_account.session")
     except Exception as e:
         await bot.send_message(chat.id ,f"**ERROR:** `{str(e)}`")
@@ -141,12 +218,11 @@ async def genStr(_, msg: Message):
         return
 
 
-@bot.on_message(filters.private & filters.command("help"))
+@bot.on_message(filters.private & filters.command("start") | filters.text)
 async def restart(_, msg: Message):
     out = f"""
-Hi, {msg.from_user.mention}. This is Pyrogram Session String Generator Bot. \
-I will give you `STRING_SESSION` for your UserBot.
-
+Hi, {msg.from_user.mention}. This is Pyrogram and telethon  Session String Generator Bot. \
+use /pyrogram for pyrogram session use /telethon for telethon session
 It needs `API_ID`, `API_HASH`, Phone Number and One Time Verification Code. \
 Which will be sent to your Phone Number.
 You have to put **OTP** in `1 2 3 4 5` this format. __(Space between each numbers!)__
