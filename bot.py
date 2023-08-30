48c3c10a8f885a23eb73914fbf4076e3e2a2c582
Elphador <96861627+Elphador@users.noreply.github.com>
2023-08-30 18:11:06 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,10 @@
 
 from pyrogram import Client
 from pyroaddon import listen
-
+import os
 API_ID = 18802415
 API_HASH = "a8993f96404fd9a67de867586b3ddc92"
-BOT_TOKEN = "5812947977:AAHke7V4wXnmHZh339SsOVrUyCUBqp3W374"
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
 bot = Client(":memory:",
              api_id=API_ID,
              api_hash=API_HASH,
